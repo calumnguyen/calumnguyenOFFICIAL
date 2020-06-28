776d8afcdd776980132e6937e3e40470ee19b461
Calum Nguyen <calumengee@calums-air.lan>
2020-06-28 00:45:12 -0700

    New commit

--- index.js
+++ index.js
@@ -7,10 +7,39 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(express.static('public'));
 
+app.set('view engine', 'ejs');
+
 //When the user clicks on the homepage, redirect to index.html
-app.get('/', function(req, res){
-res.sendFile(__dirname+'/index.html');
+
+  let eng = {
+    language: "English ",
+    about: "About",
+    connect: "Connect",
+    portfolio: "Portfolio",
+    hello: "I'm ",
+    helloTwo: " It's my pleasure to see you today!",
+    intro: "Please let me introduce myself",
+    introTwo: "In a rush? Here's my resume!"
+  };
+
+  let vnm = {
+    language: "Tiếng Việt ",
+    about: "Giới Thiệu",
+    connect: "Kết Nối",
+    portfolio: "Portfolio",
+    hello: "Mình là ",
+    helloTwo: " Thật vui khi được bạn đến thăm!",
+    intro: "Cho mình được tự giới thiệu về bản thân",
+    introTwo: "Đang vội? Đọc sơ qua Resume!"
+  };
+app.get('/vnm', function(req, res){
+  res.render('index', {language:vnm.language, about:vnm.about, connect:vnm.connect, portfolio:vnm.portfolio, hello:vnm.hello, helloTwo:vnm.helloTwo, intro:vnm.intro, introTwo:vnm.introTwo});
 })
+
+app.get('/', function(req, res){
+  res.render('index', {language:eng.language, about:eng.about, connect:eng.connect, portfolio:eng.portfolio, hello:eng.hello, helloTwo:eng.helloTwo, intro:eng.intro, introTwo:eng.introTwo});
+})
+
 
 //Confirmation for 'node index.js' or 'nodemon'
 app.listen(process.env.PORT || 3000, function(){
