8ec6ba72d8c5180790d240e0cb90ef288dd4f3e6
Calum Nguyen <calumengee@calums-air.lan>
2020-06-28 01:08:31 -0700

    add .gitignore and move views to public

--- index.js
+++ index.js
@@ -42,6 +42,6 @@ app.get('/', function(req, res){
 
 
 //Confirmation for 'node index.js' or 'nodemon'
-app.listen(process.env.PORT, function(){
+app.listen(process.env.PORT || 3000, function(){
 console.log('Port is running...');
 })
