0d594c4d93e03a64072b0048cb403e32040e3888
Calum Nguyen <calumengee@calums-air.lan>
2020-06-27 21:28:07 -0700

    Add more comments for clarity in index.js and main.js

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
+//Set Up Express and Body Parser
+
 const express = require('express');
 const bodyParser = require('body-parser');
- 
+
+//Set a static folder for CSS and Images
 const app = express();
 app.use(express.static('public'));
- 
+
+//When the user clicks on the homepage, redirect to index.html
 app.get('/', function(req, res){
 res.sendFile(__dirname+'/index.html');
 })
- 
+
+//Confirmation for 'node index.js' or 'nodemon'
 app.listen(process.env.PORT || 3000, function(){
 console.log('Port is running...');
 })
