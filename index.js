e82b4d3ff21a4e0eca2d812d6048721e30deff4a
Calum Nguyen <calumengee@calums-air.lan>
2020-06-30 12:17:12 -0700

    add footer

--- index.js
+++ index.js
@@ -19,7 +19,8 @@ app.set('view engine', 'ejs');
     hello: "I'm ",
     helloTwo: " It's my pleasure to see you today!",
     intro: "Please let me introduce myself",
-    introTwo: "In a rush? Here's my resume!"
+    introTwo: "In a rush? Here's my resume!",
+    git: "Find this website on my GitHub repositry!"
   };
 
   let vnm = {
@@ -30,15 +31,16 @@ app.set('view engine', 'ejs');
     hello: "Mình là ",
     helloTwo: " Rất vui khi được bạn đến thăm!",
     intro: "Cho mình được tự giới thiệu về bản thân",
-    introTwo: "Nếu bạn đang vội, xem qua bản tóm tắt ở đây nhé!"
+    introTwo: "Nếu bạn đang vội, xem qua bản tóm tắt ở đây nhé!",
+    git: "Xem tập tin xây dựng trang web này trong kho GitHub của mình!"
   };
 
 app.get('/vnm', function(req, res){
-  res.render('index', {language:vnm.language, about:vnm.about, connect:vnm.connect, portfolio:vnm.portfolio, hello:vnm.hello, helloTwo:vnm.helloTwo, intro:vnm.intro, introTwo:vnm.introTwo});
+  res.render('index', {language:vnm.language, about:vnm.about, connect:vnm.connect, portfolio:vnm.portfolio, hello:vnm.hello, helloTwo:vnm.helloTwo, intro:vnm.intro, introTwo:vnm.introTwo, git:vnm.git});
 })
 
 app.get('/', function(req, res){
-  res.render('index', {language:eng.language, about:eng.about, connect:eng.connect, portfolio:eng.portfolio, hello:eng.hello, helloTwo:eng.helloTwo, intro:eng.intro, introTwo:eng.introTwo});
+  res.render('index', {language:eng.language, about:eng.about, connect:eng.connect, portfolio:eng.portfolio, hello:eng.hello, helloTwo:eng.helloTwo, intro:eng.intro, introTwo:eng.introTwo, git:eng.git});
 })
 
 app.get('/vnm-hello', function(req, res){
