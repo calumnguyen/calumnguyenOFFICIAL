cbf7c006808fcfbbb504ae2eecd79d761b0b6f12
Calum Nguyen <calumengee@calums-air.lan>
2020-06-30 01:41:54 -0700

    add animation to nav bar in introduction page

---
+++ public/JS/introduction.js
@@ -0,0 +1,9 @@
+//When the user clicks on the hamburger icon
+$('.hamburger').click(function(){
+  //activate clip-path in the CSS property 'open' for '.nav-links'
+  $('.nav-links').toggleClass('open');
+  //activate fading effect by changing the opacity % in CSS property 'fade' for '.nav-tags'
+  $('.nav-tags').toggleClass('fade');
+  //when this happens, hide everything that is not nav
+  $('.not-nav').toggleClass('hide');
+});
