f89d8a309fd749bade39466c98ddb1e2acf05457
Calum Nguyen <calumengee@calums-air.lan>
2020-07-26 17:18:28 -0700

    Add Env file for mailchimp

--- index.js
+++ index.js
@@ -115,7 +115,7 @@ app.post('/connect', function(req, res) {
   const jsonData = JSON.stringify(data);
 
   const options = {
-      url: "https://us10.api.mailchimp.com/3.0/lists/a461eb083b",
+      url: process.env.MAILCHIMP_URL,
       method: "POST",
       headers: {
         "Authorization": process.env.MAILCHIMP_API
